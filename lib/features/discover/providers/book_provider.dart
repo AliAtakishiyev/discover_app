247d9963a89f0fa38b72e4adc5ec4b691d4c6bb0
aliatakishiyev <aliatakishiyev2006@gmail.com>
2026-01-25 00:31:13 +0400

    feat: book provider

---
+++ lib/features/discover/providers/book_provider.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+
+import 'package:discover_app/features/core/network/dio_client.dart';
+import 'package:discover_app/features/discover/data/models/book_model.dart';
+import 'package:discover_app/features/discover/data/repositories/book_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final BookRepositoryProvider = Provider<BookRepository>((ref) {
+  return BookRepository(DioClient());
+});
+
+final bookProvider = AsyncNotifierProvider<BookNotifier, List<BookModel>>(
+  BookNotifier.new,
+);
+
+class BookNotifier extends AsyncNotifier<List<BookModel>> {
+  late final BookRepository _repository;
+
+  @override
+  Future<List<BookModel>> build() async {
+    _repository = ref.read(BookRepositoryProvider);
+    return [];
+  }
+
+  Future<void> searchBooks(String query) async {
+    state = const AsyncLoading();
+
+    state = await AsyncValue.guard(() => _repository.getBooks(query));
+  }
+}
